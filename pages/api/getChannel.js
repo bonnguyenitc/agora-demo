41c34f4c7d1df83b7031c2e0fa7c8657e650d1d4
Nguyen Tan Thoai <thoaint@bap.jp>
2021-10-12 16:40:39 +0700

    fix name table

--- pages/api/getChannel.js
+++ pages/api/getChannel.js
@@ -5,11 +5,11 @@ import { supabaseClient } from "../../src/utils/supabaseClient";
 export default async function handler(req, res) {
   const { id } = req.query;
   const { data, error } = await supabaseClient
-    .from(WEDDINGS)
+    .from("weddings")
     .select()
     .eq("id", id);
   const { data: data1, error: error1 } = await supabaseClient
-    .from(CHANNELS)
+    .from("channels")
     .select()
     .eq("wedding_id", id);
   res.status(200).json({ data: { wedding: data?.[0], channels: data1 } });
