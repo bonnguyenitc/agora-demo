31b2ea0e8c06776107dd77e312efa28f72135c56
bonnguyenitc <ntthoaiit@gmail.com>
2021-12-15 17:19:25 +0700

    Update supabaseClient.js

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -3,5 +3,5 @@ import { createClient } from "@supabase/supabase-js";
 // Create a single supabase client for interacting with your database
 export const supabaseClient = createClient(
   "https://mfdjigekncmhpvibryiu.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNzQ4MDc1OSwiZXhwIjoxOTQzMDU2NzU5fQ.9ZIhqP7-pN5kWZRU0XF1L6ZBGiS4aW0Wi09PkKEdAdg"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMzMzNTY1MywiZXhwIjoxOTQ4OTExNjUzfQ.v6-1w3xX1v5_iz-s80hKYkIuCv-UVgQnIYIs9nEJupU"
 );
